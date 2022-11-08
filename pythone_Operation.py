ea7caeb995bdae10e253f4e1377f0096ba7ccf6e
RajibH1 <92961677+RajibH1@users.noreply.github.com>
2022-11-07 20:56:53 -0500

    Create pythone_Operation.py
    python operation

---
+++ pythone_Operation.py
@@ -0,0 +1,19 @@
+# numeric operators in python
+# + for multiplication 
+# - for substractions 
+# * for multiplication 
+# / for float division 
+# // for integer division 
+# ** for power calculation 
+# % for Modulus 
+
+x = 5
+y = 3
+
+print ("Addition of x + y = ", x+y)
+print ("subtriction of x - y = ", x-y)
+print ("multiplication of x * y = ", x*y)
+print ("float division  of x / y = ", x/y)
+print ("integer division of x // y = ", x//y)
+print ("power calculation  of x ** y = ", x**y)
+print ("Modulus of x % y = ", x%y)
