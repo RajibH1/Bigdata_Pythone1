c6c31f9c9ea00707234952e930f8a56d3c04269b
RajibH1 <92961677+RajibH1@users.noreply.github.com>
2022-11-07 21:16:27 -0500

    Update pythone_Operation.py

--- pythone_Operation.py
+++ pythone_Operation.py
@@ -17,3 +17,24 @@ print ("float division  of x / y = ", x/y)
 print ("integer division of x // y = ", x//y)
 print ("power calculation  of x ** y = ", x**y)
 print ("Modulus of x % y = ", x%y)
+# some operation in string 
+str_data = 'Rajib'
+empty_str = ''
+
+# concat operation for string 
+full_name = str_data + " "+ "Hossen"
+print("Full name = ", full_name)
+
+# if we can use - as well (minus operation does not works for string)
+
+# minus_str = "rajib" - "Hossen"
+# print("Minus str = ", minus_str)
+
+# check multiplication if works (it does not work)
+
+mul_str = "rajib" * "Hossen"
+print("mul_str str = ", mul_str)
+
+ # multipli numeric string
+mul_num_str = "rajib" * 5
+print("mul_num_str str = ", mul_num_str)
